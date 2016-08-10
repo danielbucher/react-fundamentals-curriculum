b2322687e79dcc3cc82f33852f4f60c46e8b430a
Daniel Bucher <daniel.bucher88@gmail.com>
2016-08-09 23:24:37 -0300

    Extract routes to app/routes.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-var hashHistory = ReactRouter.hashHistory;
-var Main = require('./containers/Main.jsx');
-
-var routes = (
-  <Router history={hashHistory} >
-    <Route path="/" component={Main} >
-    </Route>
-  </Router>
-);
+var routes = require('./routes.jsx');
 
 ReactDOM.render(routes, document.getElementById('app'));
