20f1a4a93d67296b36888a34bfb1b740614a3d18
Daniel Bucher <daniel.bucher88@gmail.com>
2016-08-13 14:11:00 -0300

    Change Home and Main components style

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,8 +1,21 @@
 var React = require('react');
 
+var styles = {
+  container: {
+    backgroundSize: 'cover',
+    backgroundImage: "url('app/images/pattern.svg')",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%'
+  }
+}
+
 const Home = () => {
   return (
-    <div>
+    <div style={styles.container}>
       <p>Home!</p>
     </div>
   );
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,14 +1,21 @@
 var React = require('react');
 
-const HelloWorld = (props) => {
+var styles = {
+  container: {
+    width: '100%',
+    height: '92%'
+  }
+}
+
+const Main = (props) => {
   return (
-    <div>
-      <h2>
+    <div style={styles.container}>
+      <h1>
         Main.jsx Header
-      </h2>
+      </h1>
       {props.children}
     </div>
   );
 };
 
-module.exports = HelloWorld;
+module.exports = Main;
