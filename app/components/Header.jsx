5d1c715009882247b417774bd5baa8cf139d1630
Daniel Bucher <daniel.bucher88@gmail.com>
2016-08-14 10:52:15 -0300

    Add style to Header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
 var GetCityContainer = require('../containers/GetCityContainer.jsx');
 
+const styles = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  background: 'rgba(252, 90, 44, 0.89)',
+  color: '#fff',
+  padding: 5
+};
+
 const Header = (props) => {
   return (
-    <div>
+    <div style={styles} >
+      <h2 style={{margin: 0}}>Olar, sou um app</h2>
       <GetCityContainer direction="row" />
     </div>
   );
