ae8c181450e7f690c7f8ae7aabc0a27d726fb7ae
Daniel Bucher <daniel.bucher88@gmail.com>
2016-08-14 21:43:54 -0300

    Add a getForecast function to the api helper
    Also, removes the function call to encode url component since axios already
    does that.

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -3,30 +3,42 @@ var axios = require('axios');
 var _baseUrl = "http://api.openweathermap.org/data/2.5/";
 var _APIKey = "4e868cd6ab8147f392ea311565c87c51";
 
+var getWeather = (city) => {
+  var url = getUrl("weather", city);
+  var params = getParams(city);
+
+  return fetchApiData(url, params);
+};
+
+var getForecast = (city) => {
+  var url = getUrl("forecast/daily", city);
+  var params = getParams(city);
+
+  return fetchApiData(url, params);
+};
+
 var getUrl = (type, city) => {
   return _baseUrl + type;
 };
 
 var getParams = (city) => {
   return {
-    q: encodeURIComponent(city),
+    q: city,
     APPID: _APIKey,
     type: "accurate",
     cnt: 5
   }
 };
 
-var getWeather = (city) => {
-  var url = getUrl("weather", city);
-  var params = getParams(city);
-
+var fetchApiData = (url, params) => {
   return axios.get(url, {
     params: params
   }).then(function(response) {
-    console.log(response);
+    console.log(response.data);
   }).catch(function(error) {
     console.warn(error);
   });
 };
 
 module.exports.getWeather = getWeather;
+module.exports.getForecast = getForecast;
