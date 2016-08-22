eee3aa9f6ba275aa94c29308db22a04f65e0f71b
Daniel Bucher <daniel.bucher88@gmail.com>
2016-08-21 23:43:47 -0300

    Add '/forescast/:city' route

--- app/routes.jsx
+++ app/routes.jsx
@@ -6,12 +6,14 @@ var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('./containers/Main.jsx');
 var HomeContainer = require('./containers/HomeContainer.jsx');
+var ForecastContainer = require('./containers/ForecastContainer.jsx');
 
 
 var routes = (
   <Router history={hashHistory} >
     <Route path="/" component={Main} >
       <IndexRoute component={HomeContainer} />
+      <Route path="forecast/:city" component={ForecastContainer} />
     </Route>
   </Router>
 );
