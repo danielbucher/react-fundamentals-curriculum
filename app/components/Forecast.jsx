2e8b1edea8a63a292e891e32b2a86d25945f74f2
Daniel Bucher <daniel.bucher88@gmail.com>
2016-08-28 22:31:41 -0300

    Add loading behavior to Forecast

--- app/components/Forecast.jsx
+++ app/components/Forecast.jsx
@@ -1,7 +1,83 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 
-const Forecast = (props) => {
+var styles = {
+  loadingContainer: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    fontSize: '55px'
+  },
+  loadingContent: {
+    textAlign: 'center',
+    width: '100%',
+    marginTop: '30px',
+  }
+}
+
+var Forecast = React.createClass({
+  propTypes: {
+    city: PropTypes.string.isRequired
+  },
+  getInitialState: function() {
+    return {
+      loading: true
+    };
+  },
+  render: function() {
+    return (
+      this.state.loading
+        ? <ForecastLoading text="Loading" speed={300} />
+        : <ForecastLoaded city={this.props.city} />
+    );
+  }
+});
+
+var ForecastLoading = React.createClass({
+  propTypes: {
+    text: PropTypes.string,
+    speed: PropTypes.number
+  },
+  getDefaultProps: function() {
+    return {
+      text: 'Loading',
+      speed: 300
+    };
+  },
+  getInitialState: function() {
+    return {
+      text: this.props.text
+    };
+  },
+  componentDidMount: function() {
+    var stopper = this.props.text + "...";
+
+    this.interval = setInterval(function() {
+      if (this.state.text === stopper) {
+        this.setState({
+          text: this.props.text
+        });
+      } else {
+        this.setState({
+          text: this.state.text + '.'
+        });
+      }
+    }.bind(this), this.props.speed);
+  },
+  componentWillUnmount: function() {
+    window.clearInterval(this.interval);
+  },
+  render: function() {
+    return (
+      <div style={styles.loadingContainer}>
+        <p style={styles.loadingContent}>{this.state.text}</p>
+      </div>
+    );
+  }
+});
+
+const ForecastLoaded = (props) => {
   return (
     <div>
       <h3>Forecast component</h3>
@@ -10,7 +86,7 @@ const Forecast = (props) => {
   );
 };
 
-Forecast.propTypes = {
+ForecastLoaded.propTypes = {
   city: PropTypes.string.isRequired
 }
 
