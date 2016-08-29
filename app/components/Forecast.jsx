232b884d746f4189ebcf10f42bfd1a28f92be22f
Daniel Bucher <daniel.bucher88@gmail.com>
2016-08-28 23:39:58 -0300

    Change Forecast component to render forecast data
    Also improve Forecast style.

--- app/components/Forecast.jsx
+++ app/components/Forecast.jsx
@@ -2,17 +2,43 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var Loading = require('./Loading.jsx')
 
+function puke (obj) {
+  return <pre>{JSON.stringify(obj, null, ' ')}</pre>
+}
+
+var styles = {
+  container: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
+  },
+  title: {
+    fontSize: '55px',
+    textAlign: 'center',
+    width: '100%',
+    marginTop: '30px'
+  },
+  content: {
+    width: '100%',
+    paddingLeft: '5px',
+    paddingRight: '5px'
+  }
+};
 
 var Forecast = React.createClass({
   propTypes: {
     city: PropTypes.string.isRequired,
     loading: PropTypes.bool.isRequired,
+    forecastData: PropTypes.object
   },
   render: function() {
     return (
       this.props.loading
         ? <ForecastLoading />
-        : <ForecastLoaded city={this.props.city} />
+        : <ForecastLoaded
+            city={this.props.city}
+            forecastData={this.props.forecastData} />
     );
   }
 });
@@ -25,15 +51,16 @@ const ForecastLoading = () => {
 
 const ForecastLoaded = (props) => {
   return (
-    <div>
-      <h3>Forecast component</h3>
-      <p>City: {props.city}</p>
+    <div style={styles.container}>
+      <h3 style={styles.title}>{props.city} Forecast</h3>
+      <div style={styles.content}>{puke(props.forecastData)}</div>
     </div>
   );
 };
 
 ForecastLoaded.propTypes = {
-  city: PropTypes.string.isRequired
+  city: PropTypes.string.isRequired,
+  forecastData: PropTypes.object.isRequired
 }
 
 module.exports = Forecast;
